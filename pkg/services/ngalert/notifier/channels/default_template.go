c4eb3143299af735ac34eba962335313323ac1c5
Greg Markey <greg.markey@auros.global>
2022-03-29 10:21:59 +1000

    replace default template

--- pkg/services/ngalert/notifier/channels/default_template.go
+++ pkg/services/ngalert/notifier/channels/default_template.go
@@ -12,7 +12,7 @@ import (
 const DefaultMessageTitleEmbed = `{{ template "default.title" . }}`
 
 var DefaultTemplateString = `
-{{ define "__subject" }}[{{ .CommonLabels.alertname }}] {{ .CommonAnnotations.summary }}: {{ .CommonAnnotations.instance }}{{ end }}
+{{ define "__subject" }}{{ $instance := index .Alerts 0 }}[{{ .CommonLabels.alertname }}] {{ .CommonAnnotations.summary }}: {{ $instance.Annotations.instance }}{{ end }}
 
 {{ define "__text_alert_list" }}{{ range . }}
 Value: {{ or .ValueString "[no data]" }}
